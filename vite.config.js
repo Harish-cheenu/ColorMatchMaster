b03c8369183d067042f2764e950033660968137b
Harish-cheenu <harish.cheenu@yahoo.com>
2024-08-04 18:08:58 +0530

    setup vite

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       name: 'colormatchmaster',
       filename: 'remoteEntry.js',
       exposes: {
-        './colormatchmaster': './src/app'
+        './colormatchmaster': './src/App.jsx'
       },
       shared: ['react', 'react-dom']
     })
